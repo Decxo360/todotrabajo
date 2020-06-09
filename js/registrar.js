5559b2774af692ef6df774d66ea077cec1ddae77
decxo360 <diegolundstedt99@gmail.com>
2020-06-09 17:27:22 -0400

    Modificar completamente el orden de el registrar y crear la query de la id del usuario

--- js/registrar.js
+++ js/registrar.js
@@ -1,29 +1,150 @@
+let nombre;
+let apellidoM;
+let apellidoP;
+let rut;
+let tarjeta;
+let edad;
+let correo;
+let contraseña;
+let contraseña2;
+let tipoUsuario;
+let trabajoRealizado = 0;
+let postulados = 0;
+let puntos = 0;
+let trabajoSubidos=0;
+let idusuario;
+
 document.querySelector("#btnRegistrar").addEventListener("click", function () {
+  //PERSONA
   let nombre = document.querySelector("#nombrePersona").value;
   let apellidoM = document.querySelector("#apellidoMPersona").value;
   let apellidoP = document.querySelector("#apellidoPPersona").value;
   let rut = document.querySelector("#rutPersona").value;
   let tarjeta = document.querySelector("#tarjetaPersona").value;
   let edad = document.querySelector("#edadPersona").value;
+
+  //USUARIO
+  let correo = document.querySelector("#correoPersona").value;
+  let contraseña = document.querySelector("#contraseñaPersona").value;
+  let contraseña2 =document.querySelector("#contraseña2").value;
+  let tipoUsuario = "usuario";
   
-  if(!nombre == "" && !apellidoM == "" && !apellidoP == "" && !rut == "" && !tarjeta == "" && !edad==""){
-    let nuevaPersona = {};
-    nuevaPersona.nombre = nombre;
-    nuevaPersona.apellidoM = apellidoM;
-    nuevaPersona.apellidoP = apellidoP;
-    nuevaPersona.rut = rut;
-    (nuevaPersona.edad = edad), (nuevaPersona.tarjeta = tarjeta);
-    console.log(nuevaPersona);
-    let formData = new FormData();
-    formData.append("nombre", nuevaPersona.nombre);
-    formData.append("apellidoM", nuevaPersona.apellidoM);
-    formData.append("apellidoP", nuevaPersona.apellidoP);
-    formData.append("rut", nuevaPersona.rut);
-    formData.append("edad", nuevaPersona.edad);
-    formData.append("tarjeta", nuevaPersona.tarjeta);
-    
-    axios.post("api/persona/createPersona.php", formData);
+  if(verificarCampos()){
+    iniciarRegistro()
   }else {
+  }
+
+});
+function iniciarRegistro(){
+
+  //creando a la persona
+  let nuevaPersona = {};
+  nuevaPersona.nombre = nombre;
+  nuevaPersona.apellidoM = apellidoM;
+  nuevaPersona.apellidoP = apellidoP;
+  nuevaPersona.rut = rut;
+  (nuevaPersona.edad = edad), 
+  (nuevaPersona.tarjeta = tarjeta);
+
+  //Pasando los datos al formData
+  let formData = new FormData();
+  formData.append("nombre", nuevaPersona.nombre);
+  formData.append("apellidoM", nuevaPersona.apellidoM);
+  formData.append("apellidoP", nuevaPersona.apellidoP);
+  formData.append("rut", nuevaPersona.rut);
+  formData.append("edad", nuevaPersona.edad);
+  formData.append("tarjeta", nuevaPersona.tarjeta);
+    
+  //creando al usuario
+  let nuevoUsuario = {};
+  nuevoUsuario.correo = correo;
+  nuevoUsuario.contraseña = contraseña;
+  nuevoUsuario.tipoUsuario = tipoUsuario;
+  nuevoUsuario.rut = rut;
+
+  //Pasando los datas al formData1
+  let formData2 = new FormData();
+  formData2.append("email", nuevoUsuario.correo);
+  formData2.append("pass", nuevoUsuario.contraseña);
+  formData2.append("tipousuario", nuevoUsuario.tipoUsuario);
+  formData2.append("rut", nuevoUsuario.rut);
+  
+  //Creando la experiencia del usuario
+  let nuevaExperiencia = {};
+  nuevaExperiencia.trabajoRealizado = trabajoRealizado;
+  nuevaExperiencia.postulados = postulados;
+  nnuevaExperiencia.puntos = puntos;
+  nuevaExperiencia.trabajoSubidos = trabajoSubidos;
+  nuevaExperiencia.idusuario=idusuario;
+  nuevaExperiencia.rut = rut;
+
+  //Creando el formdata de la experiencia
+  let fomrdata = new FormData();
+  fomrdata.append("esRealizado", nuevaExperiencia.trabajoRealizado);
+  fomrdata.append("esPostulado", nuevaExperiencia.postulados);
+  fomrdata.append("puntos", nuevaExperiencia.puntos);
+  fomrdata.append("esSubido", nuevaExperiencia.trabajoSubidos);
+  fomrdata.append("idusuario",nuevaExperiencia.idusuario);
+  fomrdata.append("rut", nuevaExperiencia.rut);
+
+  //crear la query del usuario
+  nuevaexp={};
+  nuevaexp.correo= correo;
+  nuevaexp.contraseña= contraseña;
+
+  //crear la query hacia la tabla usuario
+  let formdata1 = new FormData();
+  formdata1.append("email",nuevaexp.correo);
+  formdata1.append("pass",nuevaexp.contraseña);
+
+  createPersona(formData).then(resposne =>{
+    createUser(formData2).then(resposne =>{
+      createQuery(formdata1).then(response =>{
+        createExperencia(formdata1).then(response =>{
+          console.log(response.data);
+        })
+      })
+    })
+  })
+createPersona = async(formData) => {
+  const response = await axios.post("api/persona/createPersona.php", formData);
+  return await response.data;
+}
+createUser = async (formData2) => {
+  const response = await axios.post("api/usuario/createUsuario.php", formData2);
+  return response.data;
+  
+}
+createQuery = async (formdata1) =>{
+  const response = await axios.post("api/usuario/queryUsuario.php",formdata1);
+  idusuario = response.data.id;
+  return idusuario;
+}
+createExperencia = async(fomrdata) =>{
+  const response = await axios.post("api/experiencia/createExperencia.php", fomrdata);
+  return response.data;
+}
+}
+function verificarCampos(){
+
+  if 
+  (
+    !correo == "" &&
+    !contraseña == "" &&
+    !contraseña2 == "" &&
+    !rut == ""
+    && !nombre == "" 
+    && !apellidoM == "" 
+    && !apellidoP == "" 
+    && !rut == "" 
+    && !tarjeta == "" 
+    && !edad==""
+    &&(contraseña.length >= 8 && contraseña2.length >= 8)
+    &&(contraseña == contraseña2)
+  
+  ) {
+    return true; 
+  }else{
     if(nombre ==""){
       $("#invalido9").removeClass("d-none");
     }
@@ -42,122 +163,26 @@ document.querySelector("#btnRegistrar").addEventListener("click", function () {
     if(tarjeta=="No tengo"){
       $("#invalido4").removeClass("d-none");
     }
-  }
-
-});
-
-document.querySelector("#btnRegistrar").addEventListener("click", function () {
-  let correo = document.querySelector("#correoPersona").value;
-  let contraseña = document.querySelector("#contraseñaPersona").value;
-  let contraseña2 =document.querySelector("#contraseña2").value;
-  let tipoUsuario = "usuario";
-  trabajoRealizado = 0;
-  postulados = 0;
-  puntos = 0;
-  trabajoSubidos=0;
-  let rut = document.querySelector("#rutPersona").value;
-  console.log(axios.get("api/usuario/emailUsuario.php"));
-  let idpersonaa;
-  console.log(rut);
-    let error = "";
-  if (
-    !correo == "" &&
-    !contraseña == "" &&
-    !contraseña2 == "" &&
-    !rut == ""
-  ) {
-    if (contraseña.length >= 8 && contraseña2.length >= 8) {
-      if (contraseña == contraseña2) {
-
-        let nuevoUsuario = {};
-        nuevoUsuario.correo = correo;
-        nuevoUsuario.contraseña = contraseña;
-        nuevoUsuario.tipoUsuario = tipoUsuario;
-        nuevoUsuario.rut = rut;
-       
-        
-        //  let formData3 = new FormData();
-        //  let nuevaId = {};
-        //  nuevaId.rut = rut;
-        //  console.log(nuevaId);
-        //  formData3.append("rut", nuevaId.rut);
-        //  let persona = {};
-        //axios
-        //  .post("api/persona/queryPersona.php", formData3)
-        //  .then((response) => {
-        //    console.log(response);
-        //    persona = response.data;
-        //    for (let i = 0; i < persona.length; i++) {
-        //      nuevaid = {};
-        //      nuevaid = persona[i];
-        //      idpersonaa = nuevaid[0];
-        //      console.log(persona);
-        //    }
-        //  });
-        let idusuario;
-      
-        let fomrdata = new FormData();
-        nuevaexp={};
-        nuevaexp.correo= correo;
-        nuevaexp.contraseña= contraseña;
-        let formdata1 = new FormData();
-        formdata1.append("email",nuevaexp.correo);
-        formdata1.append("pass",nuevaexp.contraseña);
-        let formData2 = new FormData();
-        formData2.append("email", nuevoUsuario.correo);
-        formData2.append("pass", nuevoUsuario.contraseña);
-        formData2.append("tipousuario", nuevoUsuario.tipoUsuario);
-        formData2.append("rut", nuevoUsuario.rut);
-        axios.post("api/usuario/createUsuario.php", formData2)
-        .then(axios.post("api/usuario/queryUsuario.php",formdata1).then(response=>{
-          let usuario = response.data;
-          console.log(axios.post("api/usuario/createUsuario.php", formData2));
-          console.log(usuario);
-        //  console.log(usuario);
-          //for (let index = 0; index < usuario.length; index++) {
-          //      console.log("si funca");
-
-          //      let nu = {};
-          //      nu = usuario[index];
-          //      idusuario = nu[0];
-          //}
-         }));
-        let nuevaExperiencia = {};
-        nuevaExperiencia.trabajoRealizado = trabajoRealizado;
-        nuevaExperiencia.postulados = postulados;
-        nuevaExperiencia.puntos = puntos;
-        nuevaExperiencia.trabajoSubidos = trabajoSubidos;
-        nuevaExperiencia.idusuario=idusuario;
-        nuevaExperiencia.rut = rut;
-        fomrdata.append("esRealizado", nuevaExperiencia.trabajoRealizado);
-        fomrdata.append("esPostulado", nuevaExperiencia.postulados);
-        fomrdata.append("puntos", nuevaExperiencia.puntos);
-        fomrdata.append("esSubido", nuevaExperiencia.trabajoSubidos);
-        fomrdata.append("idusuario",nuevaExperiencia.idusuario);
-        fomrdata.append("rut", nuevaExperiencia.rut);
-        //axios.post("api/experiencia/createExperencia.php", fomrdata);
-        console.log(nuevoUsuario);
-      }else{
-          error = "No ha ingresado correctamente las contraseña";
-      }
-    } else{
-       $("#invalido2").removeClass("d-none");
-       $("#invalido2").text("La contraseña tiene que ser de largo 8 o superior");
-       $("#invalido3").removeClass("d-none");
-       $("#invalido3").text("La contraseña tiene que ser de largo 8 o superior");
-    }
-  }else {
     if (correo == "") {
-        $("#invalido1").removeClass("d-none");
-    }
-    if (contraseña == "") {
-        $("#invalido2").removeClass("d-none");
-    }
-    if (contraseña2 == "") {
-        $("#invalido3").removeClass("d-none");
-    }
-    error = "Debe ingresar todos los campos"
-    divModal = document.getElementById("staticBackdrop"); 
+      $("#invalido1").removeClass("d-none");
   }
-  console.log(error);
-});
+  if (contraseña == "") {
+      $("#invalido2").removeClass("d-none");
+  }
+  if (contraseña2 == "") {
+      $("#invalido3").removeClass("d-none");
+  }
+  if(contraseña.length < 8 ){
+    $("#invalido2").removeClass("d-none");
+    $("#invalido2").text("La contraseña tiene que ser de largo 8 o superior");
+  }
+  if (contraseña2.length <8) {
+    $("#invalido3").removeClass("d-none");
+    $("#invalido3").text("La contraseña tiene que ser de largo 8 o superior");
+  }
+  if(contraseña == contraseña2){
+    error = "No ha ingresado correctamente las contraseña";
+  }
+  return false;
+  }
+}
